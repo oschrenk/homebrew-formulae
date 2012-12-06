49088f7cfbe53a51d81565e4af41a022a78b0914
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-12-06 01:35:00 +0100

    Use sha1 instead of md5

--- geographiclib.rb
+++ geographiclib.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Geographiclib < Formula
   url 'http://downloads.sourceforge.net/project/geographiclib/distrib/GeographicLib-1.27.tar.gz'
   homepage 'http://geographiclib.sourceforge.net/'
-  md5 '9db6c042018c45ebc39d76a858c95afa'
+  sha1 '6dfdaead49267b8d96c450686f68345e5ab190f3'
 
   def install
     system "./configure", "--disable-dependency-tracking",
